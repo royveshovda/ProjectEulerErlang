b572ad38521cc8a8edcfb0b7e4877c571beb7361
Roy Veshovda <roy@veshovda.no>
2014-12-29 02:07:20 +0100

    Suggestions for improvement of 24

--- source/p024.erl
+++ source/p024.erl
@@ -24,3 +24,15 @@ assemble_number(L) ->
     L2 = lists:nth(9,L) * 10,
     L1 = lists:nth(10,L) * 1,
     L10+L9+L8+L7+L6+L5+L4+L3+L2+L1.
+
+%% TODO: Improve:
+% Explaining in my words again:
+% Permutations of n digits = n!
+% 1,000,000 = 9! * 2 + 8! * 6 + 7! * 6 + 6! * 2 + 5! * 5 + 4! * 1 + 3! * 2 + 2! * 2
+% Digits: 0123456789
+% 9! completed twice. Hence 0 and 1 for first place have been consumed. Hence it is 2 now.
+% 2 013456789
+% 8! completed 6 times. Hence 0, 1, 3, 4, 5, 6 are consumed for second place. Hence it is 8 now.
+% 28 01345679
+% With same logic repeated...
+% 1 millionth combination would be 2783915460.
