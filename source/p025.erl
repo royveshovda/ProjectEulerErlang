20968bbfb5af6e269df6547d60cbe8ff51718df3
Roy Veshovda <roy@veshovda.no>
2014-12-29 06:56:16 +0100

    Solving 25

---
+++ source/p025.erl
@@ -0,0 +1,21 @@
+-module (p025).
+-export ([run/0]).
+
+%% Correct: 4782
+
+run()->
+    fib(1,1,2,pow(10,999)).
+fib(_A, _B, Index, _Limit) when _B >= _Limit ->
+    Index;
+fib(A, B, Index, Limit) ->
+    C = A + B,
+    fib(B, C, Index+1, Limit).
+    
+pow(X, N) when is_integer(N), N >= 0 -> pow(X, N, 1);
+pow(X, N) when is_integer(N) -> 1 / pow(X, -N, 1);
+pow(X, N) when is_float(N) -> math:pow(X, N).
+
+pow(_, 0, P) -> P;
+pow(X, N, A) when N rem 2 =:= 0 ->
+    pow(X * X, N div 2, A);
+pow(X, N, A) -> pow(X, N - 1, A * X).
