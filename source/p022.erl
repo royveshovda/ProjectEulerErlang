dde12fa1709ead14330d1ce74c2d80b8eebfb1cf
Roy Veshovda <roy@veshovda.no>
2014-12-29 06:11:35 +0100

    Solving 22

--- source/p022.erl
+++ source/p022.erl
@@ -1,17 +1,30 @@
 -module (p022).
 -export ([run/0]).
 
+%% Correct: 871198282
+
 run() ->
-    {ok, Device} = file:open("../Problems/p022_names.txt", [read]),
-    Raw = file:read_line(Device),
+    Filename = "../Problems/p022_names.txt",
+    Names = open_file(Filename),
+    Sorted_list = lists:sort(Names),
+    Indexed = apply_index(Sorted_list,1,[]),
+    Weighted = lists:map(fun({V,I}) -> {V,I, alphabetical_value(V)} end, Indexed),
+    Scored = lists:map(fun({V,I,W}) -> {V,I,W,I*W} end, Weighted),
+    lists:foldl(fun({_,_,_,S}, Sum) -> Sum + S end, 0, Scored).
+
+apply_index([], _Index, Acc) ->
+    lists:reverse(Acc);
+apply_index([Head|Tail], Index, Acc) ->
+    Elem = {Head, Index},
+    apply_index(Tail, Index+1, [Elem|Acc]).
+
+
+open_file(Filename) ->
+    {ok, Device} = file:open(Filename, [read]),
+    {ok, Raw} = file:read_line(Device),
     file:close(Device),
     Tokens = string:tokens(Raw,","),
-    [string:trim(X, both, "\"") || X <- Tokens].
-    %TODO: Read file
-    %TODO: Sort names from file
-    %TODO: Rank names
-    %TODO: Sum all
-    %alphabetical_value("COLIN").
+    [string:strip(X, both, $") || X <- Tokens].    
 
 alphabetical_value(String) ->
     L = string:len(String),
