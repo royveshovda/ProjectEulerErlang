c56d1be8a5982a45be35a714048067a42f8c4e1e
Roy Veshovda <roy@veshovda.no>
2014-12-30 07:41:21 +0100

    Solving 23

---
+++ source/p023.erl
@@ -0,0 +1,26 @@
+-module (p023).
+-export ([run/0]).
+
+%% Correct: 4179871
+
+run() ->
+    Max = 28123,
+    Candidates = lists:seq(1,Max),
+    Abundant = lists:filter(fun(X) -> is_abundant(X) end, Candidates),
+    Sums = [A+B|| A <- Abundant, B <- Abundant],
+    Limited = lists:filter(fun(X) -> X =< Max end, Sums),
+    Distinct = lists:usort(Limited),
+    All = lists:sum(Candidates),
+    Abundants = lists:sum(Distinct),
+    Result = All - Abundants,
+    Result.
+
+is_abundant(N) ->
+    sum_of_factors(N) > N.
+
+sum_of_factors(N) ->
+    F = factors(N),
+    lists:sum(F) - N.
+
+factors(N) ->
+    [I || I <- lists:seq(1,trunc(N/2)), N rem I == 0]++[N].
