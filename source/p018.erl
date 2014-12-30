38ee9c0fd43010f48cf757d9ec4f419e9251f418
Roy Veshovda <roy@veshovda.no>
2014-12-30 04:30:44 +0100

    Improving 18

--- source/p018.erl
+++ source/p018.erl
@@ -1,12 +1,20 @@
 -module (p018).
--export ([run/0]).
+-export ([run/0, evaluate/1]).
 
 %% Correct: 1074
 
 run() ->
-    Tree = get_tree(),
-    Current = lists:nth(15, Tree),
-    build(Tree, Current,15,15).
+    Filename = "../Problems/p018_triangle.txt",
+    evaluate(Filename).
+
+evaluate(Filename) ->
+    Tree = open_file(Filename),
+    build(Tree).
+
+build(Tree) ->
+    Levels = length(Tree),
+    Current = lists:nth(Levels, Tree),
+    build(Tree, Current,Levels,Levels).
 
 build([[Root]|_], Current , 2, _) ->
     Root + max(lists:nth(1,Current), lists:nth(2,Current));
@@ -14,24 +22,20 @@ build(Tree, Current, Level, Depth) ->
     Next = lists:nth(Level-1, Tree),
     Length = length(Next),
     Next_Current = [lists:nth(X, Next) + max(lists:nth(X,Current), lists:nth(X+1,Current)) || X <- lists:seq(1,Length)],
-    io:format("~p~n", [Next_Current]),
     build(Tree, Next_Current,Level-1, Depth).
 
-get_tree() ->
-    Rows =
-        [[75],
-        [95, 64],
-        [17, 47, 82],
-        [18, 35, 87, 10],
-        [20, 4, 82, 47, 65],
-        [19, 1, 23, 75, 3, 34],
-        [88, 2, 77, 73, 7, 63, 67],
-        [99, 65, 4, 28, 6, 16, 70, 92],
-        [41, 41, 26, 56, 83, 40, 80, 70, 33],
-        [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
-        [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
-        [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
-        [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
-        [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
-        [ 4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]],
-    Rows.
+open_file(Filename) ->
+    {ok, Device} = file:open(Filename, [read]),
+    Result = file:read_line(Device),
+    Lines = read_lines(Device, Result, []),
+    file:close(Device),
+    Lines.
+
+read_lines(Device, {ok, Data}, Acc) ->
+    Trimmed = string:strip(Data, right, $\n),
+    Splitted = string:tokens(Trimmed, " "),
+    Ints = lists:map(fun(X) -> {I,_} = string:to_integer(X), I end, Splitted),
+    Result = file:read_line(Device),
+    read_lines(Device, Result, [Ints|Acc]);
+read_lines(_Device, eof, Acc) ->
+    lists:reverse(Acc).
