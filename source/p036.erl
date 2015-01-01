1e9e59ebc4df157b8aaf73897a60013b8ab16028
Roy Veshovda <roy@veshovda.no>
2015-01-01 12:12:58 +0100

    Solving 36

---
+++ source/p036.erl
@@ -0,0 +1,28 @@
+-module (p036).
+-export ([run/0]).
+
+%% Correct: 872187
+
+run() ->
+    Candidates = lists:seq(1,1000000),
+    Pals = lists:filter(fun(X) -> is_2_base_palindrome(X) end, Candidates),
+    lists:sum(Pals).
+
+is_2_base_palindrome(N) ->
+    
+    case is_palindrome(N) of
+        false -> false;
+        true ->
+            S = int_to_bits(N),
+            is_palindrome(S)
+    end.
+
+int_to_bits(N) ->
+    [S] = io_lib:format("~.2B", [N]),
+    S.
+
+is_palindrome(N) when is_integer(N) ->
+    Rev = list_to_integer(lists:reverse(integer_to_list(N))),
+    Rev == N;
+is_palindrome(S) ->
+    S == lists:reverse(S).
