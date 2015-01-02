f3401d7991768b8b1bd40da461210b0c53180651
Roy Veshovda <roy@veshovda.no>
2015-01-02 14:33:45 +0100

    Solving 38

--- source/p038.erl
+++ source/p038.erl
@@ -1,7 +1,22 @@
 -module (p038).
 -export ([solve/0]).
+-import (calculator, [to_digits/1, assemble_number/1]).
 
-%% Correct: ??
+%% Correct: 932718654
 
 solve() ->
-    ok.
+	Candidates = lists:map(fun(X) -> {X,expand(X)} end, lists:seq(192,9876)),
+	Results = lists:filter(fun({_N,Ds}) -> check(Ds) end, Candidates),
+	{_N,Ds} = lists:max(Results),
+	assemble_number(Ds).
+
+check(Digits) ->
+	lists:sort(Digits) == [1,2,3,4,5,6,7,8,9].
+
+expand(N) -> expand(N,1,[]).
+
+expand(_N,_Level,Digits) when length(Digits) >= 9 ->
+	Digits;
+expand(N,Level,Digits) ->
+	Ds = to_digits(Level * N),
+	expand(N, Level+1, Digits ++ Ds).
