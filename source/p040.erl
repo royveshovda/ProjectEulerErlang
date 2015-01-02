6c3a94a59b81b9dbe025d654cb440a186801b589
Roy Veshovda <roy@veshovda.no>
2015-01-02 16:24:11 +0100

    Solving 40

---
+++ source/p040.erl
@@ -0,0 +1,12 @@
+-module (p040).
+-export ([solve/0]).
+
+%% Correct: 210
+
+solve() ->
+	N = onenum(),
+	t(1,N)*t(10,N)*t(100,N)*t(1000,N)*t(10000,N)*t(100000,N)*t(1000000,N).
+
+onenum() -> lists:flatten(lists:map(fun(X) -> integer_to_list(X) end, lists:seq(1,400000))).
+	
+t(N,L) -> lists:nth(N,L) - 48.
