9f7796bb6fb70c9aa34a587958c6f6908135788a
Roy Veshovda <roy@veshovda.no>
2015-01-02 09:18:44 +0100

    Solving 97

---
+++ source/p097.erl
@@ -0,0 +1,14 @@
+-module (p097).
+-export ([solve/0]).
+-import (calculator, [pow/2]).
+
+%% Correct: 8739992577
+
+solve() ->
+	(28433*iterate_last_digits(1,2,7830457,10)+1) rem pow(10,10).
+
+iterate_last_digits(Number,_ ,0 , _) ->
+	Number;
+iterate_last_digits(Number, Base, Exp, Digits) ->
+	iterate_last_digits((Base * Number) rem pow(10, Digits), Base, Exp - 1, Digits).
+
