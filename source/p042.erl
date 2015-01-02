046a4e7cc22a132a91d288a08141c77c9b6c912e
Roy Veshovda <roy@veshovda.no>
2015-01-02 16:17:42 +0100

    Solving 42

---
+++ source/p042.erl
@@ -0,0 +1,34 @@
+-module (p042).
+-export ([solve/0]).
+-import (calculator, [alphabetical_value/1]).
+
+%% Correct: 162
+
+solve() ->
+	Filename = "../Problems/p042_words.txt",
+	Names = open_file(Filename),
+	Values = lists:map(fun(X) -> alphabetical_value(X) end, Names),
+	Max = lists:max(Values),
+	Ts = get_triangles_below(Max),
+	Triangle_words = lists:filter(fun(X) -> lists:member(X,Ts) end, Values),
+	length(Triangle_words).
+
+open_file(Filename) ->
+    {ok, Device} = file:open(Filename, [read]),
+    {ok, Raw} = file:read_line(Device),
+    file:close(Device),
+    Tokens = string:tokens(Raw,","),
+    [string:strip(X, both, $") || X <- Tokens].
+
+get_triangles_below(Max) ->
+	get_triangles_below(1,Max,[]).
+
+get_triangles_below(N,Max,Acc) ->
+	T = get_triangle(N),
+	if
+		T>Max -> Acc;
+		T=<Max -> get_triangles_below(N+1,Max,[T|Acc])
+	end.
+
+get_triangle(N) -> 
+	trunc(1/2*N*(N+1)).
