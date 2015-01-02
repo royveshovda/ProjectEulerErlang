2a1329ed4e4beea434455aa8cda4ee88de609db7
Roy Veshovda <roy@veshovda.no>
2015-01-02 15:11:52 +0100

    Solving 43

---
+++ source/p043.erl
@@ -0,0 +1,20 @@
+-module (p043).
+-export ([solve/0]).
+-import (calculator, [assemble_number/1, permute/1]).
+
+%% Correct: 16695334890
+
+solve() ->
+	Candidates = permute(lists:seq(0,9)),
+	Results = lists:filter(fun(X) -> check(X) end, Candidates),
+	Numbers = lists:map(fun(Ds) -> assemble_number(Ds) end, Results),
+	lists:sum(Numbers).
+
+%% Faster to check the biggest numbers first
+check(Digits) ->
+	Checks = [{8,17},{7,13},{6,11},{5,7},{4,5},{3,3},{2,2}],
+	lists:foldl(fun({Start, Divisor}, Acc) -> check_single(Digits,Start,Divisor,Acc) end,true,Checks).
+
+check_single(_Digits, _Start, _Divisor, false) -> false;
+check_single(Digits, Start, Divisor, Acc) ->
+	Acc and (assemble_number(lists:sublist(Digits,Start,3)) rem Divisor == 0).
