021822fba3638b877f5174ed03f81f8a176ad628
Roy Veshovda <roy@veshovda.no>
2015-01-03 04:45:52 +0100

    Solving 47

---
+++ source/p047.erl
@@ -0,0 +1,20 @@
+-module (p047).
+-export ([solve/0]).
+
+%%Link: https://projecteuler.net/problem=47
+%% Correct: 134043
+
+solve() ->
+    prime_server:start_link(200000),
+    Ans = find(2*3*5*7, 4, 0),
+    prime_server:stop(),
+    Ans.
+
+find(X, Cnt, Cnt) -> X-Cnt;
+find(X, Cnt, Fnd) ->
+    case prime_server:count_factors(X) =:= Cnt of
+        true ->
+            find(X+1, Cnt, Fnd+1);
+        false ->
+            find(X+1, Cnt, 0)
+    end.
