2c735285195eec48e5180d24485e95dd6ef6d1fa
Roy Veshovda <roy@veshovda.no>
2015-01-03 10:54:07 +0100

    Solve 56

---
+++ source/p056.erl
@@ -0,0 +1,15 @@
+-module (p056).
+-export ([solve/0]).
+-import (calculator, [pow/2,to_digits/1]).
+
+%%Link: https://projecteuler.net/problem=56
+%% Correct: 972
+
+solve() ->
+    Candidates = [ digital_sum(pow(A, B)) || A <- lists:seq(1,99), B <- lists:seq(1,99) ],
+    lists:max(Candidates).
+
+digital_sum(N) ->
+    Ds = to_digits(N),
+    lists:sum(Ds).
+    %lists:foldl(fun(D, Acc) -> (D-$0) + Acc end, 0, integer_to_list(N)).
