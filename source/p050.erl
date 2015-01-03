8ccb706ec52171cf486de0bce505a4ac130bcb27
Roy Veshovda <roy@veshovda.no>
2015-01-03 05:07:59 +0100

    Start 50 and 51

---
+++ source/p050.erl
@@ -0,0 +1,8 @@
+-module (p050).
+-export ([solve/0]).
+
+%%Link: https://projecteuler.net/problem=50
+%% Correct: ??
+
+solve() ->
+    ok.
---
+++ source/p051.erl
@@ -0,0 +1,8 @@
+-module (p051).
+-export ([solve/0]).
+
+%%Link: https://projecteuler.net/problem=51
+%% Correct: ??
+
+solve() ->
+    ok.
