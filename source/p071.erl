6e06ea7ff863a591fddbf02f11b5edcd0955b944
Roy Veshovda <roy@veshovda.no>
2015-01-04 07:56:04 +0100

    Start 71

---
+++ source/p071.erl
@@ -0,0 +1,17 @@
+-module (p071).
+-export ([solve/0]).
+-import (my_math, [gcd/2]).
+
+solve() ->
+	L = genlist(),
+	erlang:display("Stage 2"),
+	find(L).
+
+find([{A,_},{3,7}|_]) -> A;
+find(L) -> find(tl(L)).
+
+genlist() ->
+	O = lists:seq(1,1000000),
+	Raw = [ {N, D} || N <- O, D <- O, N < D, N/D =< 3/7],
+	erlang:display("Stage 1"),
+	lists:filter(fun({N,D}) -> gcd(N,D) =:= 1 end, Raw).
