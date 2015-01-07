52edb11814ddd40df1f98f7277ea007a1831709f
Roy Veshovda <roy@veshovda.no>
2015-01-07 05:53:45 +0100

    Continue 54

--- source/p054.erl
+++ source/p054.erl
@@ -5,15 +5,22 @@
 %% Correct: ??
 
 solve() ->
-    Line = "8C TS KC 9H 4S 7D 2S 5D 3S AC",
-    string:tokens(Line, " ").
-    %Card = "8C",
-    %parse_card(Card).
+    
+    L1 = "5H 5C 6S 7S KD 2C 3S 8S 8D TD",
+    L2 = "5D 8C 9S JS AC 2C 5C 7D 8S QH",
+    L3 = "2D 9C AS AH AC 3D 6D 7D TD QD",
+    L4 = "4D 6S 9H QH QC 3D 6D 7H QD QS",
+    L5 = "2H 2D 4C 4D 4S 3C 3D 3S 9S 9D",
+    Lines = [L1,L2,L3,L4,L5],
+    Hands = lists:map(fun(L) -> parse_line(L) end, Lines),
+    lists:map(fun({H1,H2}) -> is_hand1_winner(H1,H2) end, Hands).
 
-
-    %H1 = lists:sort([{5,hearts},{5,clubs}, {6,spades}, {7,spades}, {13,diamonds}]),
-    %H2 = lists:sort([{2,clubs},{3,spades}, {8,spades}, {8,diamonds}, {10,diamonds}]),
-    %is_hand1_winner(H1, H2).
+parse_line(Line) ->
+    Cards = string:tokens(Line, " "),
+    All = lists:map(fun(X) -> parse_card(X) end, Cards),
+    H1 = lists:sublist(All,5),
+    H2 = lists:sublist(All, 6,5),
+    {H1,H2}.
 
 parse_card([Value,Suit]) ->
     {parse_value(Value), parse_suit(Suit)}.
@@ -43,146 +50,172 @@ parse_suit(S) ->
         $H -> hearts
     end.    
 
+is_hand1_winner(H1, H2) ->
+    R1 = rank_hand(lists:reverse(lists:sort(H1))),
+    R2 = rank_hand(lists:reverse(lists:sort(H2))),
+    %io:format("H1: ~p~n", [R1]),
+    %io:format("H2: ~p~n", [R2]),
+    is_hand1_winner_sorted(R1,R2).
 
-is_hand1_winner(H1,H2) ->
-    H1_sorted = lists:sort(H1),
-    H2_sorted = lists:sort(H2),
-    R1 = get_rank_for_hand(H1_sorted),
-    R2 = get_rank_for_hand(H2_sorted),
-    print_rank(R1),
-    print_rank(R2),
-    case R1 == R2 of
-        false ->
-            R1 >= R2;
-        true ->
-            %%TODO: This is wrong. Needs to consider highest card, not sum
-            Sum1 = lists:foldl(fun({V,_S}, Acc) -> Acc + V end, 0, H1_sorted),
-            erlang:display(Sum1),
-            Sum2 = lists:foldl(fun({V,_S}, Acc) -> Acc + V end, 0, H2_sorted),
-            erlang:display(Sum2),
-            Sum1 >= Sum2
-    end.
+is_hand1_winner_sorted({R1,_,_}, {R2,_,_}) when R1 > R2 -> {true,1};
+is_hand1_winner_sorted({R1,_,_}, {R2,_,_}) when R1 < R2 -> {false,2};
+is_hand1_winner_sorted({R1,RC1,NRC1}, {R2,RC2,NRC2}) when R1 == R2 ->
+    is_hand1_winner_same_rank({RC1, NRC1}, {RC2, NRC2}).
 
+is_hand1_winner_same_rank({[],[]}, {[],[]}) -> {false,3}; %% Draw
+is_hand1_winner_same_rank({[{V1,_}|_], _} , {[{V2,_}|_], _}) when V1 > V2 -> {true,4};
+is_hand1_winner_same_rank({[{V1,_}|_], _} , {[{V2,_}|_], _}) when V1 < V2 -> {false,5};
+is_hand1_winner_same_rank({[{V1,_}|RCs1], NRC1} , {[{V2,_}|RCs2], NRC2}) when V1 == V2 ->
+    is_hand1_winner_same_rank({RCs1, NRC1} , {RCs2, NRC2});
+is_hand1_winner_same_rank({[], [{V1,_}|_]} , {[], [{V2,_}|_]}) when V1 > V2 -> {true,6};
+is_hand1_winner_same_rank({[], [{V1,_}|_]} , {[], [{V2,_}|_]}) when V1 < V2 -> {false,7};
+is_hand1_winner_same_rank({[], [{V1,_}|NRCs1]} , {[], [{V2,_}|NCRs2]}) when V1 == V2 ->
+    is_hand1_winner_same_rank({[], NRCs1} , {[], NCRs2}).
 
-print_rank(Rank) ->
-    case Rank of
-        1 -> erlang:display("You have nothing");
-        2 -> erlang:display("One pair");
-        3 -> erlang:display("Two pairs");
-        4 -> erlang:display("Three of a kind");
-        5 -> erlang:display("Straight");
-        6 -> erlang:display("Flush");
-        7 -> erlang:display("Full house");
-        8 -> erlang:display("Four of a kind");
-        9 -> erlang:display("Straight flush");
-        10 -> erlang:display("Royal stright flush")
-    end.
+% Royal Flush: Rank = 10
+rank_hand([{_,S1} = C1, {_,S2}= C2, {_,S3} = C3, {_,S4} = C4, {_,S5} = C5])
+            when
+                (S1 == S2)
+                andalso (S1 == S3)
+                andalso (S1 == S4)
+                andalso (S1 == S5) ->
+    Ranked = [C1, C2, C3, C4, C5],
+    Not = [],
+    {10, Ranked, Not};
 
-get_rank_for_hand(Hand) ->
-    case is_royal_flush(Hand) of
-        true -> 10;
-        false ->
-            case is_straight_flush(Hand) of
-                true -> 9;
-                false ->
-                    case is_four_of_a_kind(Hand) of
-                        true -> 8;
-                        false ->
-                            case is_full_house(Hand) of
-                                true -> 7;
-                                false ->
-                                    case is_flush(Hand) of
-                                        true -> 6;
-                                        false -> 
-                                            case is_straight(Hand) of
-                                                true -> 5;
-                                                false ->
-                                                    case is_three_of_a_kind(Hand) of
-                                                        true -> 4;
-                                                        false ->
-                                                            case is_two_pairs(Hand) of
-                                                                true -> 3;
-                                                                false ->
-                                                                    case is_one_pair(Hand) of
-                                                                        true -> 2;
-                                                                        false -> 1 % Only high cards
-                                                                    end
-                                                            end
-                                                    end
-                                            end
-                                    end
-                            end
-                    end
+% Straight Flush: Rank = 9
+rank_hand([{V1, S1} = C1, {V2, S2}= C2, {V3, S3} = C3, {V4, S4} = C4, {V5, S5} = C5])
+            when
+                (S1 == S2)
+                andalso (S1 == S3)
+                andalso (S1 == S4)
+                andalso (S1 == S5)
+                andalso (V1 == (V2+1))
+                andalso (V2 == (V3+1))
+                andalso (V3 == (V4+1))
+                andalso (V4 == (V5+1)) ->
+    Ranked = [C1, C2, C3, C4, C5],
+    Not = [],
+    {9, Ranked, Not};
 
-            end
-    end.
+% Four of a kind: Rank = 8
+rank_hand([{V1,_} = C1, {V2,_}= C2, {V3,_} = C3, {V4,_} = C4, C5])
+            when ((V1 == V2) and (V1 == V3) and (V1 == V4)) ->
+    Ranked = [C1, C2, C3, C4],
+    Not = [C5],
+    {8, Ranked, Not};
 
-% Rank 10
-is_royal_flush([{V1, S1}, {V2, S2}, {V3, S3}, {V4, S4}, {V5, S5}]) ->
-    case is_same_suit(S1,S2,S3,S4,S5) of
-        false -> false;
-        true -> 
-            V1 == 10 andalso
-            V2 == 11 andalso
-            V3 == 12 andalso
-            V4 == 13 andalso
-            V5 == 14
-    end.
+% Four of a kind: Rank = 8
+rank_hand([C1, {V2,_}= C2, {V3,_} = C3, {V4,_} = C4, {V5,_} = C5])
+            when ((V2 == V3) and (V2 == V4) and (V2 == V5)) ->
+    Ranked = [C2, C3, C4, C5],
+    Not = [C1],
+    {8, Ranked, Not};
 
-%   Rank 9
-is_straight_flush([{V1, S1}, {V2, S2}, {V3, S3}, {V4, S4}, {V5, S5}]) ->
-    case is_same_suit(S1,S2,S3,S4,S5) of
-        false -> false;
-        true -> 
-            V1 == V2 + 1 andalso
-            V2 == V3 + 1 andalso
-            V3 == V4 + 1 andalso
-            V4 == V5 + 1
-    end.
+% Full house: Rank = 7
+rank_hand([{V1,_} = C1, {V2,_}= C2, {V3,_} = C3, {V4,_} = C4, {V5,_} = C5])
+            when ((V1 == V2) and (V1 == V3) and (V4 == V5)) ->
+    Ranked = [C1, C2, C3, C4, C5],
+    Not = [],
+    {7, Ranked, Not};
 
-%   Rank 8
-is_four_of_a_kind([{V1, _S1}, {V2, _S2}, {V3, _S3}, {V4, _S4}, {V5, _S5}]) ->
-    ((V1 == V2) and (V1 == V3) and (V1 == V4)) or
-    ((V2 == V3) and (V2 == V4) and (V2 == V5)). 
+% Full house: Rank = 7
+rank_hand([{V1,_} = C1, {V2,_}= C2, {V3,_} = C3, {V4,_} = C4, {V5,_} = C5])
+            when ((V1 == V2) and (V3 == V4) and (V3 == V5)) ->
+    Ranked = [C3, C4, C5, C1, C2], % Order very important
+    Not = [],
+    {7, Ranked, Not};
 
-%   Rank 7
-is_full_house([{V1, _S1}, {V2, _S2}, {V3, _S3}, {V4, _S4}, {V5, _S5}]) ->
-    ((V1 == V2) and (V1 == V3) and (V4 == V5)) or
-    ((V1 == V2) and (V3 == V4) and (V3 == V5)).
+% Flush: Rank = 6
+rank_hand([{_,S1} = C1, {_,S2}= C2, {_,S3} = C3, {_,S4} = C4, {_,S5} = C5])
+            when (S1 == S2) andalso (S1 == S3) andalso (S1 == S4) andalso (S1 == S5) ->
+    Ranked = [C1, C2, C3, C4, C5],
+    Not = [],
+    {6, Ranked, Not};
 
-%   Rank 6
-is_flush([{_V1, S1}, {_V2, S2}, {_V3, S3}, {_V4, S4}, {_V5, S5}]) ->
-    is_same_suit(S1,S2,S3,S4,S5).
+% Straight: Rank = 5
+rank_hand([{V1,_} = C1, {V2,_}= C2, {V3,_} = C3, {V4,_} = C4, {V5,_} = C5])
+            when
+                (V1 == (V2 + 1)) and
+                (V2 == (V3 + 1)) and
+                (V3 == (V4 + 1)) and
+                (V4 == (V5 + 1)) ->
+    Ranked = [C1, C2, C3, C4, C5],
+    Not = [],
+    {5, Ranked, Not};
 
-%   Rank 5
-is_straight([{V1, _S1}, {V2, _S2}, {V3, _S3}, {V4, _S4}, {V5, _S5}]) ->
-    (V2 == (V1 + 1)) and
-    (V3 == (V2 + 1)) and
-    (V4 == (V3 + 1)) and
-    (V5 == (V4 + 1)).
+% Three of a kind: Rank = 4
+rank_hand([{V1,_} = C1, {V2,_}= C2, {V3,_} = C3, C4, C5])
+            when ((V1 == V2) and (V1 == V3)) ->
+    Ranked = [C1, C2, C3],
+    Not = [C4, C5],
+    {4, Ranked, Not};
 
-%   Rank 4
-is_three_of_a_kind([{V1, _S1}, {V2, _S2}, {V3, _S3}, {V4, _S4}, {V5, _S5}]) ->
-    ((V1 == V2) and (V1 == V3)) or
-    ((V2 == V3) and (V2 == V4)) or
-    ((V3 == V4) and (V3 == V5)).
+% Three of a kind: Rank = 4
+rank_hand([C1, {V2,_}= C2, {V3,_} = C3, {V4,_} = C4, C5])
+            when ((V2 == V3) and (V2 == V4)) ->
+    Ranked = [C2, C3, C4],
+    Not = [C1, C5],
+    {4, Ranked, Not};
 
-%   Rank 3
-is_two_pairs([{V1, _S1}, {V2, _S2}, {V3, _S3}, {V4, _S4}, {V5, _S5}]) ->
-    ((V1 == V2) and (V3 == V4)) or
-    ((V1 == V2) and (V4 == V5)) or
-    ((V2 == V3) and (V4 == V5)).
+% Three of a kind: Rank = 4
+rank_hand([C1, C2, {V3,_} = C3, {V4,_} = C4, {V5,_} = C5])
+            when ((V3 == V4) and (V3 == V5)) ->
+    Ranked = [C3, C4, C5],
+    Not = [C1, C2],
+    {4, Ranked, Not};
 
-%   Rank 2
-%   One Pair: Two cards of the same value.
-is_one_pair([{V1, _S1}, {V2, _S2}, {V3, _S3}, {V4, _S4}, {V5, _S5}]) ->
-    (V1 == V2) or 
-    (V2 == V3) or 
-    (V3 == V4) or 
-    (V4 == V5).
+% Two pairs: Rank = 3
+rank_hand([{V1,_} = C1, {V2,_}= C2, {V3,_} = C3, {V4,_} = C4, C5])
+            when ((V1 == V2) and (V3 == V4)) ->
+    Ranked = [C1, C2, C3, C4],
+    Not = [C5],
+    {3, Ranked, Not};
 
-is_same_suit(S1, S2, S3, S4, S5) ->
-    S1 == S2 andalso
-    S1 == S3 andalso
-    S1 == S4 andalso
-    S1 == S5.
+% Two pairs: Rank = 3
+rank_hand([{V1,_} = C1, {V2,_}= C2, C3, {V4,_} = C4, {V5,_} = C5])
+            when ((V1 == V2) and (V4 == V5)) ->
+    Ranked = [C1, C2, C4, C5],
+    Not = [C3],
+    {3, Ranked, Not};
+
+% Two pairs: Rank = 3
+rank_hand([C1, {V2,_}= C2, {V3,_} = C3, {V4,_} = C4, {V5,_} = C5])
+            when ((V2 == V3) and (V4 == V5)) ->
+    Ranked = [C2, C3, C4, C5],
+    Not = [C1],
+    {3, Ranked, Not};
+
+% One pair: Rank = 2
+rank_hand([{V1,_} = C1, {V2,_}= C2, C3, C4, C5])
+            when (V1 == V2) ->
+    Ranked = [C1, C2],
+    Not = [C3, C4, C5],
+    {2, Ranked, Not};
+
+% One pair: Rank = 2
+rank_hand([C1, {V2,_}= C2, {V3,_} = C3, C4, C5])
+            when (V2 == V3) ->
+    Ranked = [C2, C3],
+    Not = [C1, C4, C5],
+    {2, Ranked, Not};
+
+% One pair: Rank = 2
+rank_hand([C1, C2, {V3,_} = C3, {V4,_} = C4, C5])
+            when (V3 == V4) ->
+    Ranked = [C3, C4],
+    Not = [C1, C2, C5],
+    {2, Ranked, Not};
+
+% One pair: Rank = 2
+rank_hand([C1, C2, C3, {V4,_} = C4, {V5,_} = C5])
+            when (V4 == V5) ->
+    Ranked = [C4, C5],
+    Not = [C1, C2, C3],
+    {2, Ranked, Not};
+
+% Nothing: Rank = 1
+rank_hand(Hand) ->
+    Ranked = [],
+    Not = Hand,
+    {1, Ranked, Not}.
