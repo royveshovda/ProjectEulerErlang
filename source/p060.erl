7a26ac27323a2b64875f6223f48d80d941b99eee
Roy Veshovda <roy@veshovda.no>
2015-01-08 18:07:46 +0100

    Start 60

--- source/p060.erl
+++ source/p060.erl
@@ -1,7 +1,18 @@
 -module (p060).
 -export ([solve/0]).
+-import (helper, [to_digits/1, assemble_number/1]).
 
 solve() ->
     prime_server:start_link(20000),
     Primes = prime_server:sieve(20000),
-    Primes.
+    Pair_candidates = [{A,B}|| A <- Primes, B <- Primes, B>A],
+    Pairs = lists:filter(fun({A,B}) -> is_pair(A,B) end, Pair_candidates),
+    length(Pairs).
+    %is_pair(7,109).
+
+is_pair(A,B) ->
+	As = to_digits(A),
+	Bs = to_digits(B),
+	AB = assemble_number(As ++ Bs),
+	BA = assemble_number(Bs ++ As),
+	prime_server:is_prime(AB) andalso prime_server:is_prime(BA).
