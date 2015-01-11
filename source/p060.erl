077716abb3f636f01583697fa859da6b2a33347f
Roy Veshovda <roy@veshovda.no>
2015-01-11 06:32:58 +0100

    Work on 60

--- source/p060.erl
+++ source/p060.erl
@@ -5,12 +5,18 @@
 solve() ->
     Filename_pairs = "p060_pairs.data",
     Filename_sets = "p060_sets.data",
-    {Pairs, Candidate_sets} = get_data(Filename_pairs, Filename_sets),
-    %Valid_sets = lists:filter(fun(S) -> verify_set(S, Pairs) end, Candidate_sets),
-    %Sets_with_sum = lists:map(fun(L) -> {lists:sum(L), L} end, Valid_sets),
-    %{S,_} = lists:min(Sets_with_sum),
-    %S.
-    length(Candidate_sets).
+    Map = get_sets_as_map(Filename_pairs, Filename_sets),
+    %Ns = maps:keys(Map),
+
+    S3 = maps:get(3,Map),
+    S7 = maps:get(7,Map),
+    S673 = maps:get(673,Map),
+    Si = sets:intersection([S3,S7,S673]),
+    sets:to_list(Si).
+
+    %TODO: Loop through all numbers, and try tu build intersections of size 5 or more
+
+    %Map -> {N,Set}
 
 verify_set([A,B,C,D,E], Pairs) ->
     Checks = [{A,B},{A,C},{A,D},{A,E},{B,C},{B,D},{B,E},{C,D},{C,E},{D,E}],
@@ -23,19 +29,20 @@ verify_set([A,B,C,D,E], Pairs) ->
     end,
     R.
 
-get_data(Filename_pairs, Filename_sets) ->
+get_sets_as_map(Filename_pairs, Filename_sets) ->
     case file:read_file_info(Filename_sets) of
         {ok,_} ->
             {ok, Binary} = file:read_file(Filename_sets),
-            binary_to_term(Binary);
+            {_Pairs, Candidates} = binary_to_term(Binary),
+            maps:from_list(Candidates);
         _ ->
 
             Pairs = get_pairs(Filename_pairs),
-            Candidates = generate_set_candidates(Pairs),
+            Candidates = generate_sets(Pairs),
             Data = {Pairs, Candidates},
             Binary = term_to_binary(Data),
             ok = file:write_file(Filename_sets, Binary),
-            Data
+            maps:from_list(Candidates)
     end.
 
 get_pairs(Filename) ->
@@ -50,16 +57,22 @@ get_pairs(Filename) ->
             Pairs
    	end.
 
-generate_set_candidates(Pairs) ->
+generate_sets(Pairs) ->
     Ns = get_all_numbers(Pairs),
-    %io:format("Numbers: ~p~n", [length(Ns)]),
-    Set_candidates = permute_5(Ns, Pairs),
-    %Set_candidates = get_permutations(Ns),
-    Internal_sorted = lists:map(fun(L) -> lists:sort(L) end, Set_candidates),
-    lists:usort(Internal_sorted).
+    lists:map(fun(N) -> {N, get_set(N, Pairs)} end, Ns).
   
-permute_5(L, Pairs) ->
-    [[A,B,C,D,E] || A <- L, B <- L--[A], C <- L--[A]--[B], D <- L--[A]--[B]--[C], E <- L--[A]--[B]--[C]--[D], verify_set([A,B,C,D,E],Pairs)].
+get_set(N,Pairs) ->
+    get_set(N, Pairs, []).
+
+get_set(_N, [], Acc) ->
+    sets:from_list(lists:sort(Acc));
+    %lists:sort(Acc);
+get_set(N, [{N,B}|Rest], Acc) ->
+    get_set(N, Rest, [B|Acc]);
+get_set(N, [{A,N}|Rest], Acc) ->
+    get_set(N, Rest, [A|Acc]);
+get_set(N, [{_A,_B}|Rest], Acc) ->
+    get_set(N, Rest, Acc).
 
 get_all_numbers(Pairs) ->
     Ns = lists:foldl(fun({A,B}, Acc) -> [A,B|Acc] end, [], Pairs),
